7c5e93110b572f97c61110c467f467bc9068dad5
iz <2131601562@qq.com>
2024-09-06 15:13:28 +0800

    fix some error

--- src/utils/subsrt.ts
+++ src/utils/subsrt.ts
@@ -6,11 +6,14 @@ export function parseSrt(content: string, video: HTMLVideoElement) {
     for (let i = 0; i < lines.length; i++) {
         if(!lines[i]) continue;
         const res = lines[i].match(/^(\d+)\r?\n(\d{2}):(\d{2}):(\d{2}),(\d{3})\s*-->\s*(\d{2}):(\d{2}):(\d{2}),(\d{3})\s*([\w\W]*)\s*$/);
-        if (!res || res[1] != (i + 1).toString()) throw new Error(`Invalid SRT format at group ${i + 1}`);
+        if (!res) throw new Error(`Invalid SRT format at group ${i + 1}`);
+        
         const start = parseInt(res[2]) * 3600 + parseInt(res[3]) * 60 + parseInt(res[4]) + parseInt(res[5]) / 1000,
             end = parseInt(res[6]) * 3600 + parseInt(res[7]) * 60 + parseInt(res[8]) + parseInt(res[9]) / 1000;
+        if (start == end) continue; // 跳过空字幕
         if (start > end) throw new Error(`Invalid SRT timestrap at group ${i + 1}`);
-        if(start == end) continue; // 跳过空字幕
+        if (res[1] != (i + 1).toString() && res[1] != i.toString())
+            throw new Error(`Invalid SRT index at group ${i + 1}`);
 
         // 尝试解析部分ASS Tag
         const cue = new VTTCue(start, end, '');
